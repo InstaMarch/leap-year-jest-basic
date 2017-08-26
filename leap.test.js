345b61151e0d69611306ba1444292904dccf890c
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-08-26 17:30:59 -0600

    adding a test for a leap year

--- leap.test.js
+++ leap.test.js
@@ -5,3 +5,8 @@ test("a year that is not a leap year", function () {
     store.dispatch(changeYear(2015));
     expect(store.getState().isLeap).toBe(false);
 });
+
+test("a year that is a leap year", function () {
+    store.dispatch(changeYear(2016));
+    expect(store.getState().isLeap).toBe(true);
+});
